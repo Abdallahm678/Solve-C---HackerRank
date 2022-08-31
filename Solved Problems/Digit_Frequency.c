5cf4ef727f731584fcf7f283bb6707d5ea1ea762
Abdullah Mohamed <48365456+Abdallahm678@users.noreply.github.com>
2022-08-31 14:06:33 +0200

    Update Digit_Frequency.c

--- Solved Problems/Digit_Frequency.c
+++ Solved Problems/Digit_Frequency.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int max_len=1001;
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int j,count;
+    char s[max_len],i;
+    scanf("%s",s);
+    int string_length = strlen(s);
+    for(i=48;i<58;i++){ //Asci value of zero = 48
+        count=0;
+        for(j=0 ; j<string_length ; j++){
+            if (i==s[j])
+            count ++;
+        }
+        printf("%d ", count);
+    }  
+    return 0;
+}
 
